e4faf91adcf1dbb75b630ba5e32206774cc106c7
RohitSolu <rohitbastola.solu@gmail.com>
2022-03-24 21:13:52 +0545

    Third commit

--- Himal_darshan/settings.py
+++ Himal_darshan/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 import os
 
@@ -129,3 +129,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Activate Django-HEroku
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Himal_darshan.wsgi
