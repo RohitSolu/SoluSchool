72e705636bf7e4b19c4f997bdcfa9e821a385314
RohitSolu <rohitbastola.solu@gmail.com>
2022-03-27 17:07:14 +0545

    bash-3

--- Himal_darshan/settings.py
+++ Himal_darshan/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -122,17 +122,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #Activate Django-HEroku
-django_heroku.settings(locals())
+
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+django_heroku.settings(locals())
 
